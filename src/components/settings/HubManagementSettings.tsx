ddbbe61cf5116384cdce1315f67498c5045f15e4
MKTripsy <janchristiansentripolca@gmail.com>
2026-06-19 17:45:39 +0800

    admin username avoid spaces

--- src/components/settings/HubManagementSettings.tsx
+++ src/components/settings/HubManagementSettings.tsx
@@ -386,7 +386,7 @@ export default function HubManagementSettings() {
                   <div>
                     <label className="block text-sm font-medium text-gray-700 mb-1">Username</label>
                     <input type="text" value={adminForm.username}
-                      onChange={e => setAdminForm({ ...adminForm, username: e.target.value })}
+                      onChange={e => setAdminForm({ ...adminForm, username: e.target.value.replace(/\s/g, '') })}
                       className="w-full border border-gray-300 rounded-lg px-3 py-2 text-black focus:outline-none focus:ring-2 focus:ring-[#FF6347]"
                     />
                   </div>
